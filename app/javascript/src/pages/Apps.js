8b1e5a257ad1335efe14dab2c7561689f3b59af5
nzelenskyy <nickolai.zelenskyy@gmail.com>
2020-10-23 08:24:05 +0300

    change names

--- app/javascript/src/pages/Apps.js
+++ app/javascript/src/pages/Apps.js
@@ -79,7 +79,7 @@ function App ({ dispatch, loading }) {
               <div className="rounded-lg h-96">
                 <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                   <div className="sm:text-center lg:text-left">
-                    <img src={logo} alt="logo" />
+                    <img src="https://accfarm.com/themes/accfarm/assets/images/accfarm_logo.svg" alt="logo" />
 
                     <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
                       {I18n.t('home.welcome')} <br className="xl:hidden" />
@@ -92,22 +92,13 @@ function App ({ dispatch, loading }) {
                     </p>
 
                     <div className="mt-5 sm:mt-8 mb-8 sm:flex sm:justify-center lg:justify-start">
-                      <div className="rounded-md shadow">
+                      <div className="rounded-md shadow" style="display:none;">
                         <Link
                           to="/apps/new"
                           className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                         >
                           {I18n.t('home.create_new')}
                         </Link>
-                      </div>
-                      <div className="mt-3 sm:mt-0 sm:ml-3">
-                        <a
-                          href="https://dev.chaskiq.io"
-                          target={'blank'}
-                          className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
-                        >
-                          {I18n.t('home.go_to_doc')}
-                        </a>
                       </div>
                     </div>
                   </div>
